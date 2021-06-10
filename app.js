363304f346ac4f60e39767e7cc5e7d797da76b4b
rf300545 <ARTB0805@carbocloro.com.br>
2021-06-10 00:03:25 -0300

    falta mas responsive

--- app.js
+++ app.js
@@ -30,6 +30,12 @@ app.listen(process.env.PORT || 3000, function() {
 app.get ("/", (req,res) => {
     res.sendFile (path.resolve (__dirname, "./views/index.html"));
 })
+app.get ("/register.html", (req,res) => {
+    res.sendFile (path.resolve (__dirname, "./views/register.html"));
+})
+app.get ("/login.html", (req,res) => {
+    res.sendFile (path.resolve (__dirname, "./views/login.html"));
+})
 
 
 
